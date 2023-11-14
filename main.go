9d1065b31e07d6da53231f4955316a4978dc77d6
Vojtěch Struhár <vojtechstruhar@gmail.com>
2023-11-14 19:14:29 +0100

    Image uploading

--- main.go
+++ main.go
@@ -3,13 +3,18 @@ package main
 import (
 	"context"
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	_ "github.com/mattn/go-sqlite3"
 	"html/template"
+	"io"
 	"net/http"
+	"os"
+	"path/filepath"
 	"strconv"
+	"time"
 )
 
 var router *chi.Mux
@@ -41,6 +46,8 @@ func main() {
 	*/
 	router.Use(ChangeMethod)
 	router.Get("/", GetAllArticles)
+	router.Post("/upload", UploadHandler) // Add this
+	router.Get("/images/*", ServeImages)  // Add this
 	router.Route("/articles", func(r chi.Router) {
 		r.Get("/", NewArticle)
 		r.Post("/", CreateArticle)
@@ -159,4 +166,57 @@ func DeleteArticle(w http.ResponseWriter, r *http.Request) {
 	catch(err)
 
 	http.Redirect(w, r, "/", http.StatusFound)
+}
+
+func UploadHandler(w http.ResponseWriter, r *http.Request) {
+	const MAX_UPLOAD_SIZE = 10 << 20 // Set the max upload size to 10 MB
+	r.Body = http.MaxBytesReader(w, r.Body, MAX_UPLOAD_SIZE)
+	if err := r.ParseMultipartForm(MAX_UPLOAD_SIZE); err != nil {
+		http.Error(w, "The uploaded file is too big. Please choose a file that's less than 10MB in size", http.StatusBadRequest)
+		return
+	}
+
+	file, fileHeader, err := r.FormFile("file")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	defer file.Close()
+
+	// Create the uploads folder if it doesn't already exist
+	err = os.MkdirAll("./images", os.ModePerm)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Create a new file in the uploads directory
+	filename := fmt.Sprintf("/images/%d%s", time.Now().UnixNano(), filepath.Ext(fileHeader.Filename))
+	dst, err := os.Create("." + filename)
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	defer dst.Close()
+
+	// Copy the uploaded file to  the specified destination
+	_, err = io.Copy(dst, file)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	fmt.Println(filename)
+	response, _ := json.Marshal(map[string]string{"location": filename})
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	w.Write(response)
+}
+
+func ServeImages(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.URL)
+	fs := http.StripPrefix("/images/", http.FileServer(http.Dir("./images")))
+	fs.ServeHTTP(w, r)
 }
